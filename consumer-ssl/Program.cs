6054b1b401d115b0cdcb435d07baa058e68c0b1e
André Canuto <andrecanuto83@gmail.com>
2024-04-26 09:25:38 -0300

    Feat: poc kafka with ssl

---
+++ consumer-ssl/Program.cs
@@ -0,0 +1,46 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace consumer_ssl
+{
+    static class Program
+    {
+        private static readonly int numberOfRecords = 99;
+
+        static void Main()
+        {
+            var baseDirectory = Environment.CurrentDirectory.Split("bin");
+            Console.WriteLine(baseDirectory);
+
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = Consumer.BootstrapServers,
+                GroupId = Consumer.GroupId,
+                ClientId = Consumer.ClientId,
+                SaslMechanism = SaslMechanism.Plain,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslUsername = Consumer.SaslUsername,
+                SaslPassword = Consumer.SaslPassword,
+                SslEndpointIdentificationAlgorithm = SslEndpointIdentificationAlgorithm.None,
+                SslCaLocation = baseDirectory[0] + Consumer.SslCaLocation,
+                SslCertificateLocation = baseDirectory[0] + Consumer.SslCertificateLocation,
+                SslKeyLocation = baseDirectory[0] + Consumer.SslKeyLocation,
+            };
+
+            var builder = new ConsumerBuilder<string, string>(config);
+
+            using var consumer = builder.Build();
+            consumer.Subscribe(Consumer.Topic);
+            int init = 0;
+
+            while (init <= numberOfRecords)
+            {
+                var record = consumer.Consume(TimeSpan.FromMilliseconds(1000));
+
+                Console.WriteLine($"{init} - DateTime:{DateTime.Now} | Key:{record?.Message?.Key} | Value:{record?.Message?.Value}");
+
+                init++;
+            }
+        }
+    }
+}
---
+++ producer-ssl/Program.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+using System;
+
+namespace producer_ssl
+{
+    static class Program
+    {
+        private static readonly int numberOfRecords = 99;
+
+        static void Main()
+        {
+            var baseDirectory = Environment.CurrentDirectory.Split("bin");
+            Console.WriteLine(baseDirectory[0]);
+
+            var config = new ProducerConfig
+            {
+                BootstrapServers = Producer.BootstrapServers,
+                ClientId = Producer.ClientId,
+                SaslMechanism = SaslMechanism.Plain,
+                SecurityProtocol = SecurityProtocol.SaslSsl,
+                SaslUsername = Producer.SaslUsername,
+                SaslPassword = Producer.SaslPassword,
+                SslEndpointIdentificationAlgorithm = SslEndpointIdentificationAlgorithm.None,
+                SslCaLocation = baseDirectory[0] + Producer.SslCaLocation,
+                SslCertificateLocation = baseDirectory[0] + Producer.SslCertificateLocation,
+                SslKeyLocation = baseDirectory[0] + Producer.SslKeyLocation
+            };
+
+            var builder = new ProducerBuilder<string, string>(config);
+
+            using var producer = builder.Build();
+
+            int init = 0;
+
+            while (init <= numberOfRecords)
+            {
+                producer.Produce(Producer.Topic, new Message<string, string>
+                {
+                    Key = $"{init}",
+                    Value = $"Andre 198{init}"
+                });
+
+                init++;
+            }
+        }
+    }
+}
